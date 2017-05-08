d3d72b44ff7713cdf6db2ceca5ca3ef21be0829e
Jared Luedtke and nick wise <nick@>
2017-05-08 10:05:19 -0700

    Add Spec and README.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Places You've Been
+#### Specifications
+  1. Gather user input
+  2. Store input in variables
+  3. Make Object for locations user has been that stores generic info such as Location, Landmark, Time, and Notes
+  4. Store all objects into new variable
+  5. on click of location tab. Show all attributes
+  6. Place proper answer as text in proper class.
+  7. Show result 
+#### This website demonstrates the use of Objects.
+
+
+#### By Nick Wise and Jared Luedtke
+
+## Description
+    This is a basic application that gathers user input of previous vacations and uses Object to gather similar attributes such as location, landmark,time, and notes. And displays them as clickable items.
+## Setup/Installation Requirements
+
+ Download clone from  Github account to your Desktop. open all files in your favorite text editor to edit the code.
+
+## Known Bugs
+
+No know bugs
+
+## Support and contact details
+
+contact me at nickmwise@gmail.com for support.
+
+## Technologies Used
+
+HTML, CSS, Bootstrap, JavaScript, and JQuery.
+
+
+### License
+
+This project is licensed under the MIT License
+
+Copyright (c) 2017
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+function Places (location, landmark, time, notes) {
+  this.inputLocation = location;
+  this.inputLandmark = landmark;
+  this.inputTime = time;
+  this.inputNotes = notes;
+};
+
+Places.prototype.fullList = function () {
+  return this.inputLocation + " || " + this.inputLandmark + " || " + this.inputTime + " || " + this.inputNotes;
+};
+
+
+
+
+
 $(function(){
   $("form#new-place").submit(function(event){
     event.preventDefault();
@@ -5,5 +20,25 @@ $(function(){
     var inputLandmark = $("input#landmark").val();
     var inputTime = $("input#time").val();
     var inputNotes = $("input#notes").val();
+
+    var newPlace = new Places(inputLocation, inputLandmark, inputTime, inputNotes);
+    console.log(newPlace);
+
+    $("#listLocation").append("<li><span class='location'>" + newPlace.inputLocation + "</span></li>");
+
+    $(".location").last().click(function() {
+      $("#show-location").show();
+      $(".locations").text(newPlace.inputLocation);
+      $(".landmark").text(newPlace.inputLandmark);
+      $(".year").text(newPlace.inputTime);
+      $(".notes").text(newPlace.inputNotes);
   });
+  $(".location").val("");
+  $(".landmark").val("");
+  $(".year").val("");
+  $(".notes").val("");
+
+  });
+
+
 });
